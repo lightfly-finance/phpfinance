f2da213ba9c26925855a8927fdabe113471e401f
twn39 <twn39@163.com>
2019-05-17 20:20:34 +0800

    update

--- src/Stock/Stock.php
+++ src/Stock/Stock.php
@@ -1,12 +1,9 @@
 <?php
-
-
 namespace Monster\Finance\Stock;
 
 
 use function iter\map;
 use function iter\toArray;
-use function iter\toIter;
 use Monster\Finance\HttpClientInterface;
 
 class Stock
@@ -20,17 +17,106 @@ class Stock
 
     static $HS300_API = 'http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?num=300&sort=symbol&asc=1&node=hs300';
 
+    static $STOCK_HISTORY_API = 'http://finance.sina.com.cn/realstock/company';
+
+    static $DAILY_HISTORY_API = 'http://quotes.money.163.com/service/chddata.html';
+
+    /**
+     * Stock constructor.
+     * @param HttpClientInterface $httpClient
+     */
     public function __construct(HttpClientInterface $httpClient)
     {
-
         $this->httpClient = $httpClient;
     }
 
+    /**
+     * 数据源： http://quotes.money.163.com/trade/lsjysj_600004.html?year=2019&season=1
+     * @param $symbol
+     * @param $dateFrom
+     * @param $dateTo
+     * @return array
+     */
+    public function dailyHistory($symbol, $dateFrom, $dateTo)
+    {
+        $queryString = http_build_query([
+            'code' => "0$symbol",
+            'start' => date('Ymd', strtotime($dateFrom)),
+            'end' => date('Ymd', strtotime($dateTo)),
+        ]);
+
+        $url = self::$DAILY_HISTORY_API.'?'.$queryString.'&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP';
+
+        return toArray($this->dailyKData($url));
+    }
+
+    /**
+     * 每日历史数据
+     *
+     * @param $url
+     * @return \Generator
+     */
+    private function dailyKData($url)
+    {
+        $res = $this->httpClient->get($url);
+
+        $data = mb_convert_encoding($res, 'UTF-8', 'GB2312');
+
+        $data = explode("\r\n", $data);
+
+        foreach ($data as $item) {
+            if (!empty($item)) {
+                $row = explode(',', $item);
+                $row[1] = trim($row[1], "'");
+                yield $row;
+            }
+        }
+    }
+
+    /**
+     * 获取股票每日复权历史数据
+     *
+     * @param $symbol string 前面有前缀sh或者sz
+     * @param $type
+     * @return array
+     */
+    public function history($symbol, $type)
+    {
+        $res = $this->httpClient->get(self::$STOCK_HISTORY_API."/$symbol/$type.js");
+
+        $pattern = '/data:\{([0-9_ ",:.]+)\}/';
+
+        preg_match($pattern, $res, $matches);
+        $data = explode(',',$matches[1]);
+
+        $tmp = [];
+        foreach ($data as $item) {
+            $pattern = '/^_(\d+)_(\d+)_(\d+):"([0-9.]+)"/';
+            preg_match($pattern, $item, $result);
+
+            $key = $result[1].'-'.$result[2].'-'.$result[3];
+            $tmp[$key] = $result[4];
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * 数据源： http://vip.stock.finance.sina.com.cn/mkt/#hs300
+     * 沪深300成分股
+     *
+     * @return array
+     */
     public function HS300()
     {
         return toArray($this->getHS300Page());
     }
 
+    /**
+     * 获取沪深300分页数据
+     *
+     * @return \Generator
+     */
     public function getHS300Page()
     {
         $page = 1;
