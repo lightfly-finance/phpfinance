9325091f9fca04701fb5258fcb70c2f3ffe703e3
twn39 <twn39@163.com>
2019-05-31 15:39:20 +0800

    update

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -11,11 +11,21 @@ class HttpClient implements HttpClientInterface
 {
     private $httpClient;
 
+    /**
+     * HttpClient constructor.
+     * @param Client $client
+     */
     public function __construct(Client $client)
     {
         $this->httpClient = $client;
     }
 
+    /**
+     * @param $url
+     * @param array $options
+     * @return string
+     * @throws HttpException
+     */
     public function get($url, $options = [])
     {
         $res = $this->httpClient->get($url, $options);
