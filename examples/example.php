39fcd2cc80e3eb00ee737139781ce82c85f93cad
twn39 <twn39@163.com>
2019-06-17 14:23:41 +0800

    添加fixer汇率数据

--- examples/example.php
+++ examples/example.php
@@ -1,6 +1,7 @@
 <?php
 
 use GuzzleHttp\Client;
+use Lightfly\Finance\Fixer\Fixer;
 use Lightfly\Finance\Fund\Fund;
 use Lightfly\Finance\HttpClient;
 use Lightfly\Finance\Stock\Stock;
@@ -9,8 +10,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $httpClient = new HttpClient(new Client());
 
-$stock = new Stock($httpClient);
-$data = $stock->HSHKRealTimeTrade();
+$fixer = new Fixer($httpClient, '29c96e229e98942d24888c0b5c8e9050');
+$data = $fixer->all();
 
-var_dump($data);
+
+
+
 
