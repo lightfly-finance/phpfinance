58d47b226f50f5bf8b1dea768993cddcd928b472
twn39 <twn39@163.com>
2019-09-16 13:47:56 +0800

    add proxy http client

---
+++ src/CURLHttpProxyClient.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Lightfly\Finance;
+
+use GuzzleHttp\Client;
+use Lightfly\Finance\Exception\HttpException;
+
+class CURLHttpProxyClient implements HttpClientInterface
+{
+    private $httpClient;
+
+    private $httpProxy;
+
+    private $noProxy;
+
+    /**
+     * HttpClient constructor.
+     * @param Client $client
+     * @param string $httpProxy
+     * @param array $noProxy
+     */
+    public function __construct(Client $client, string $httpProxy, $noProxy = [])
+    {
+        $this->httpClient = $client;
+        $this->httpProxy = $httpProxy;
+        $this->noProxy = $noProxy;
+    }
+
+    /**
+     * @param $url
+     * @param array $options
+     * @return string
+     * @throws HttpException
+     */
+    public function get($url, $options = [])
+    {
+        $options['proxy'] = [
+            'http' => $this->httpProxy,
+            'no' => $this->noProxy,
+        ];
+        $res = $this->httpClient->get($url, $options);
+
+        $code = $res->getStatusCode();
+        if ($code == 456) {
+            throw new HttpException('Rate limit forbidden.');
+        }
+
+        return $res->getBody()->getContents();
+    }
+
+}
